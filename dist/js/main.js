803ad2dcbc584a96518cf7e70411456d6955970d
Matthew Philp <mattp84@Matthews-MBP.lan>
2018-08-19 12:48:42 +0100

    Menu overly added

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll("nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
 
@@ -14,16 +14,16 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuNav.classList.add("close");
-    menuBranding.classList.add("close");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuNav.classList.remove("close");
-    menuBranding.classList.remove("close");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     showMenu = false;
